ce89a3ac4cd43a4331d145486e9d6d131904da83
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-08-31 10:53:34 +0530

    added operation

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ console.log(b);
 const add = (a,b) => a+b;
 console.log(`Sum of two numbers: ${add(a,b)}`);
 
+const subtract = (a,b) => a-b;
+console.log(`Sum of two numbers: ${subtract(a,b)}`);
+
 const multiply = (a,b) => a*b;
 console.log(`Multiplication of two numbers: ${multiply(a,b)}`);
 
+const divide = (a,b) => a/b;
+console.log(`Multiplication of two numbers: ${divide(a,b)}`);
