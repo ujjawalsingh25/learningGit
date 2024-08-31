ceedb5f24e78eda0fd3ba14ee03950562dc64ea3
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-08-31 10:44:37 +0530

    Modified logs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(b);
 
 const add = (a,b) => a+b;
 const sum = add(a,b);
-console.log(sum);
+console.log(`Sum of two numbers: ${sum}`);
 
 const multiply = (a,b) => a+b;
 const mul = add(a,b);
